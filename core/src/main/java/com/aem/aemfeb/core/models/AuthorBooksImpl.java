8eecdbbfd16078b292513b86e8872e3aeaafe3dd
raj-amirishetti <raj.amirishetti@gmail.com>
2022-08-20 17:12:27 +0530

    multifield

---
+++ core/src/main/java/com/aem/aemfeb/core/models/AuthorBooksImpl.java
@@ -0,0 +1,73 @@
+package com.aem.aemfeb.core.models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Model(adaptables = SlingHttpServletRequest.class,
+	defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL	)
+public class AuthorBooksImpl {
+	
+	
+	 private static final Logger LOG = LoggerFactory.getLogger(AuthorBooksImpl.class);
+	 
+	 
+	@Inject
+    Resource componentResource;
+
+    @ValueMapValue
+    @Default(values = "AEM")
+    private String authorname;
+
+    @ValueMapValue	
+    private List<String> books;
+
+
+   
+    public String getAuthorName() {
+        return authorname;
+    }
+
+    
+    public List<String> getAuthorBooks() {
+        if(books!=null){
+            return new ArrayList<String>(books);
+        }else{
+            return Collections.emptyList();
+        }
+    }
+    
+    public List<Map<String, String>> getBookDetailsWithMap() {
+        List<Map<String, String>> bookDetailsMap=new ArrayList<>();
+        try {
+            Resource bookDetail=componentResource.getChild("bookdetailswithmap");
+            if(bookDetail!=null){
+                for (Resource book : bookDetail.getChildren()) {
+                    Map<String,String> bookMap=new HashMap<>();
+                    bookMap.put("bookname",book.getValueMap().get("bookname",String.class));
+                    bookMap.put("booksubject",book.getValueMap().get("booksubject",String.class));
+                    bookMap.put("publishyear",book.getValueMap().get("publishyear",String.class));
+                    bookDetailsMap.add(bookMap);
+                }
+            }
+        }catch (Exception e){
+            LOG.info("\n ERROR while getting Book Details {} ",e.getMessage());
+        }
+        LOG.info("\n SIZE {} ",bookDetailsMap.size());
+        return bookDetailsMap;
+    }
+
+    
+    
+}
