05c2ec0420133f5001ef41a3e1842682643d5a75
raj-amirishetti <raj.amirishetti@gmail.com>
2023-04-17 16:04:48 +0530

    semantic tags

--- core/src/main/java/com/aem/aemfeb/core/servlets/JsonDataDropdownServlet.java
+++ core/src/main/java/com/aem/aemfeb/core/servlets/JsonDataDropdownServlet.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.servlet.Servlet;
@@ -29,7 +28,7 @@ import org.apache.sling.commons.json.JSONObject;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory;      
 
 import com.adobe.granite.ui.components.ds.DataSource;
 import com.adobe.granite.ui.components.ds.SimpleDataSource;
@@ -38,72 +37,75 @@ import com.day.cq.commons.jcr.JcrConstants;
 
 @Component(service = Servlet.class, property = { Constants.SERVICE_DESCRIPTION + "=Json Data in dynamic Dropdown",
 		"sling.servlet.paths=" + "/bin/jsonDataDropdown", "sling.servlet.methods=" + HttpConstants.METHOD_GET })
-public class JsonDataDropdownServlet extends SlingSafeMethodsServlet{
+public class JsonDataDropdownServlet extends SlingSafeMethodsServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(JsonDataDropdownServlet.class);
 
-    transient ResourceResolver resourceResolver;
-    transient Resource pathResource;
-    transient ValueMap valueMap;
-    transient List<Resource> resourceList;
+	transient ResourceResolver resourceResolver;
+	transient Resource pathResource;
+	transient ValueMap valueMap;
+	transient List<Resource> resourceList;
 
-    @Override
-    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+	@Override
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
 
-        resourceResolver = request.getResourceResolver();
-        pathResource = request.getResource();
-        resourceList = new ArrayList<>();
+		resourceResolver = request.getResourceResolver();
+		pathResource = request.getResource();
+		resourceList = new ArrayList<>();
 
-        try {
-            /* Getting AEM Tags Path given on datasource Node */
-            String jsonDataPath = Objects.requireNonNull(pathResource.getChild("datasource")).getValueMap().get("jsonDataPath", String.class);
-            assert jsonDataPath != null;
-            //Getting Tag Resource using JsonData
-            Resource jsonResource = request.getResourceResolver().getResource(jsonDataPath + "/" + JcrConstants.JCR_CONTENT);
-            assert jsonResource != null;
-            //Getting Node from jsonResource
-            Node jsonNode = jsonResource.adaptTo(Node.class);
-            assert jsonNode != null;
-            //Converting input stream to JSON Object
-            InputStream inputStream = jsonNode.getProperty("jcr:data").getBinary().getStream();
+		try {
+			/* Getting AEM Tags Path given on datasource Node */
+			String jsonDataPath = Objects.requireNonNull(pathResource.getChild("datasource")).getValueMap()
+					.get("jsonDataPath", String.class);
+			assert jsonDataPath != null;
+			// Getting Tag Resource using JsonData
+			Resource jsonResource = request.getResourceResolver()
+					.getResource(jsonDataPath + "/" + JcrConstants.JCR_CONTENT);
+			assert jsonResource != null;
+			// Getting Node from jsonResource
+			Node jsonNode = jsonResource.adaptTo(Node.class);
+			assert jsonNode != null;
+			// Converting input stream to JSON Object
+			InputStream inputStream = jsonNode.getProperty("jcr:data").getBinary().getStream();
 
-            StringBuilder stringBuilder = new StringBuilder();
-            String eachLine;
-            assert inputStream != null;
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+			StringBuilder stringBuilder = new StringBuilder();
+			String eachLine;
+			assert inputStream != null;
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
-            while ((eachLine = bufferedReader.readLine()) != null) {
-                stringBuilder.append(eachLine);
-            }
+			while ((eachLine = bufferedReader.readLine()) != null) {
+				stringBuilder.append(eachLine);
+			}
 
-            JSONObject jsonObject = new JSONObject(stringBuilder.toString());
-            Iterator<String> jsonKeys = jsonObject.keys();
-            //Iterating JSON Objects over key
-            while (jsonKeys.hasNext()) {
-                String jsonKey = jsonKeys.next();
-                String jsonValue = jsonObject.getString(jsonKey);
+			JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+			Iterator<String> jsonKeys = jsonObject.keys();
 
-                valueMap = new ValueMapDecorator(new HashMap<>());
-                valueMap.put("value", jsonKey);
-                valueMap.put("text", jsonValue);
-                resourceList.add(new ValueMapResource(resourceResolver, new ResourceMetadata(), "nt:unstructured", valueMap));
-            }
+			// Iterating JSON Objects over key
+			while (jsonKeys.hasNext()) {
+				String jsonKey = jsonKeys.next();
+				String jsonValue = jsonObject.getString(jsonKey);
 
-            /*Create a DataSource that is used to populate the drop-down control*/
-            DataSource dataSource = new SimpleDataSource(resourceList.iterator());
-            request.setAttribute(DataSource.class.getName(), dataSource);
+				valueMap = new ValueMapDecorator(new HashMap<>());
+				valueMap.put("value", jsonKey);
+				valueMap.put("text", jsonValue);
+				resourceList.add(
+						new ValueMapResource(resourceResolver, new ResourceMetadata(), "nt:unstructured", valueMap));
 
-        } catch (JSONException | IOException | RepositoryException e) {
-            LOGGER.error("Error in Json Data Exporting : {}", e.getMessage());
-        }
-    }
-	
+			}
+
+			/* Create a DataSource that is used to populate the drop-down control */
+			DataSource dataSource = new SimpleDataSource(resourceList.iterator());
+			request.setAttribute(DataSource.class.getName(), dataSource);
+
+		} catch (JSONException | IOException | RepositoryException e) {
+			LOGGER.error("Error in Json Data Exporting : {}", e.getMessage());
+		}
+	}
 
 }
-
