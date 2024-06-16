c2fd63612a8d3c9b683ab89f757584710683e65b
Rajashekar <raj.amirishetti@gmail.com>
2024-06-16 14:42:07 +0530

    rebase

--- core/src/main/java/com/aem/aemfeb/core/models/TestModelClass.java
+++ core/src/main/java/com/aem/aemfeb/core/models/TestModelClass.java
@@ -1,5 +1,9 @@
 package com.aem.aemfeb.core.models;
 
+<<<<<<< HEAD
+=======
+import com.day.cq.wcm.msm.commons.BaseAction;
+>>>>>>> de3c911 (practice1)
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -9,8 +13,15 @@ import org.apache.sling.models.annotations.Model;
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class TestModelClass {
 
+<<<<<<< HEAD
     public String getName(){
         return "I'm from TestModelClass";
+=======
+
+
+    public String getName(){
+        return "I'm from TestModel";
+>>>>>>> de3c911 (practice1)
     }
 
 }
